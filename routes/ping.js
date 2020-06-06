bea0dccc1e6938974e663ca88aba3573138363d9
Abhishek9717 <abhishekkumargupta100@gmail.com>
2020-06-06 16:03:00 +0530

    Apis Done

--- routes/ping.js
+++ routes/ping.js
@@ -6,11 +6,7 @@ router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json());
 
 router.get('/',(req,res)=>{
-    res.body = {
-        "success":"true",
-        "status":"OK"
-    }
-    res.render(res.body);
+    res.status(200).json({"success":true});  
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const axios = require('axios');
+const https = require('https')
 
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json());
 
-var posts = {}
-router.get('',(req,res)=>{
-    console.log(req);
-    const tags = req.body.tags;
-    tags.forEach((tag,index)=>{
+router.get('/',(req,res)=>{
+
+    var posts = [];
+    var newPosts = [];
+    if(req.query){
+        if(req.query.tags == null){
+            res.status(404);
+            res.json({"error": "Tag parameter is required"});
+        }
+        var taglist = req.query.tags.split(',');
+        var sortBy = req.query.sortBy || id;
+        var direction = req.query.direction || asc;
+
+        console.log(taglist+"\n"+sortBy+"\n"+direction);
         
-    })
-    const sortBy = req.body.sortBy ;
-    const direction = req.body.direction;
+        function compare(a,b){
+            if(direction=="asc"){
+                if(a.sortBy < b.sortBy){
+                    return 1;
+                }
+                else if(a.sortBy > b.sortBy){
+                    return -1;
+                }
+            }
+            else if(direction=="desc"){
+                if(a.sortBy > b.sortBy){
+                    return 1;
+                }
+                else if(a.sortBy < b.sortBy){
+                    return -1;
+                }
+            }
+            return 0;
+        }
+
+        taglist.forEach((tag,i)=>{
+            let url = `https://hatchways.io/api/assessment/blog/posts?tag=${tag}`;
+
+            axios.get(url).then(response=>{
+                
+                console.log("URL is :" + url);
+                
+                newPosts = response.data ? response.data.posts : [];
+                posts.push.apply(posts,newPosts);
+                
+                console.log(posts);
+                
+                if(i==taglist.length - 1){
+                    jsonObject = posts.map(JSON.stringify);
+                    uniqueSet = new Set(jsonObject);
+                    posts = Array.from(uniqueSet).map(JSON.parse);
+                    console.log(sortBy + " , " +direction)
+                    if(direction == "desc"){
+                        posts.sort((a,b)=>b.sortBy - a.sortBy)
+                    }else{
+                        posts.sort((a,b)=>a.sortBy - b.sortBy)
+                    }
+                    //posts.sort(compare)
+                    res.status(200).json({"posts":posts});
+                }
+            });
+        })
+   } 
+   
+   
+    
 
 });
 
